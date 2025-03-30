1de9f5e563d983d10ec61c7d2c906edb492a0d55
adrianocavs <adriano.carvalho@live.com>
2025-03-30 01:48:30 -0300

    Finalizando as implementações de sábado (sem aula)
    Regra de negócio principal implementada, finalizada e testada nos mais diversos condicionais e internacionalização EN-US/PT-BR
    
    Falta modo paisagem
    Testes de mesa com valores padrão (<, zero e >)
    12,12
    8,98
    5,95
    5,26
    
    Converter uso de decimais de . para ,

--- app/src/main/java/com/cavstecnologia/econofuel/MainActivity.kt
+++ app/src/main/java/com/cavstecnologia/econofuel/MainActivity.kt
@@ -2,19 +2,23 @@ package com.cavstecnologia.econofuel
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.cavstecnologia.econofuel.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding;
     private var pressedButton : Int = 0;
+    private var firstFuel : Int = 0;
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,12 +42,36 @@ class MainActivity : AppCompatActivity() {
             btSelectFuelOnClick();
         }
 
+        binding.btCalculateFuel.setOnClickListener {
+            var fuelConsumption1 : String = binding.etFuelConsumption1?.text.toString().trim();
+            var fuelConsumption2 : String = binding.etFuelConsumption2?.text.toString().trim();
+            var fuelCost1 : String = binding.etFuelCost1.text.toString().trim();
+            var fuelCost2 : String = binding.etFuelCost2.text.toString().trim();
+
+            if (fuelConsumption1.isEmpty() || fuelConsumption2.isEmpty() || fuelCost1.isEmpty() || fuelCost2.isEmpty()){
+                Toast.makeText(this, R.string.empty_field, Toast.LENGTH_LONG).show();
+            }else{
+                if(firstFuel <= 1){ //se o primeiro combustível for gasolina ou não tiver sido selecionado
+                    val gasolinePrice : Double = binding.etFuelCost1.text.toString().trim().toDouble();
+                    val ethanolPrice : Double = binding.etFuelCost2.text.toString().trim().toDouble();
+                    val gasolineAutonomy : Double = binding.etFuelConsumption1?.text.toString().trim().toDouble();
+                    val ethanolAutonomy : Double = binding.etFuelConsumption2?.text.toString().trim().toDouble();
+                    calculateBestFuel(gasolinePrice, ethanolPrice,gasolineAutonomy, ethanolAutonomy);
+                }else if (firstFuel == 2){ //se o primeiro combustível for etanol
+                    val gasolinePrice : Double = binding.etFuelCost2.text.toString().trim().toDouble();
+                    val ethanolPrice : Double = binding.etFuelCost1.text.toString().trim().toDouble();
+                    val gasolineAutonomy : Double = binding.etFuelConsumption2?.text.toString().trim().toDouble();
+                    val ethanolAutonomy : Double = binding.etFuelConsumption1?.text.toString().trim().toDouble();
+                    calculateBestFuel(gasolinePrice, ethanolPrice,gasolineAutonomy, ethanolAutonomy);
+                }
+            }
+        }
+
     }
 
     fun btSelectFuelOnClick() {
         val intent = Intent(this, ListActivity::class.java);
-        startActivity(intent);
-
+        //startActivity(intent);
         getResult.launch(intent);
     }
 
@@ -59,23 +87,116 @@ class MainActivity : AppCompatActivity() {
                 if (returnCod == 1) { //se o selecionado foi o 1º item (gasolina)
                     binding.etFuelConsumption1?.setText(gasolineAutonomy.toString());
                     binding.etFuelConsumption2?.setText(ethanolAutonomy.toString());
+
+                    setFuelTextGasolineFirst();
                 } else {
                     binding.etFuelConsumption1?.setText(ethanolAutonomy.toString());
                     binding.etFuelConsumption2?.setText(gasolineAutonomy.toString());
+
+                    setFuelTextEthanolFirst();
                 }
                 //binding.etFuelConsumption1.setText(returnCod)
             } else {//caso tenha sido pressionado o segundo botão
                 if (returnCod == 1) { //faz o inverso do anterior, veja que o etFuelConsumption foi trocado
                     binding.etFuelConsumption2?.setText(gasolineAutonomy.toString());
                     binding.etFuelConsumption1?.setText(ethanolAutonomy.toString());
+
+                    setFuelTextEthanolFirst();
                 } else {
                     binding.etFuelConsumption2?.setText(ethanolAutonomy.toString());
                     binding.etFuelConsumption1?.setText(gasolineAutonomy.toString());
+
+                    setFuelTextGasolineFirst();
                 }
             }
 
         }
     }//termina getResult
+
+    private fun setFuelTextGasolineFirst(){
+        binding.tvFuelConsumption1.setText(R.string.gasoline_consumption);
+        binding.tvFuelConsumption2.setText(R.string.ethanol_consumption);
+        binding.tvFuelValue1.setText(R.string.gasoline_cost);
+        binding.tvFuelValue2.setText(R.string.ethanol_cost)
+
+        firstFuel = 1;
+        binding.etFuelCost1.requestFocus();
+    }
+
+    private fun setFuelTextEthanolFirst(){
+        binding.tvFuelConsumption2.setText(R.string.gasoline_consumption);
+        binding.tvFuelConsumption1.setText(R.string.ethanol_consumption);
+        binding.tvFuelValue2.setText(R.string.gasoline_cost);
+        binding.tvFuelValue1.setText(R.string.ethanol_cost)
+
+        firstFuel = 2;
+        binding.etFuelCost1.requestFocus();
+    }
+
+
+
+    private fun calculateBestFuel(gasolinePrice : Double, ethanolPrice : Double, gasolineAutonomy : Double, ethanolAutonomy : Double) {
+        val fuelAutonomyRatio : Double = ethanolAutonomy/gasolineAutonomy;
+        val fuelPriceRatio : Double = ethanolPrice/gasolinePrice;
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+
+        val efficiency : Int = ((Math.abs(fuelAutonomyRatio - fuelPriceRatio)*100)/fuelAutonomyRatio).roundToInt();
+
+        var resultMsg : String;
+
+        if (firstFuel == 0){ //se não foi utilizado o botão "select" pra selecionar gasolina ou etanol
+            builder.setMessage(R.string.no_fuel_specified).setTitle(R.string.notice).setPositiveButton(R.string.ok) { dialog, which ->
+                var internDialog : AlertDialog = builder.create();
+                if (fuelPriceRatio <= fuelAutonomyRatio){  //Se o custo do etanol for de aproximadamente 0,7 ou 70% do valor da gasolina, saberá com o calculo anterior
+                    if (efficiency == 0) resultMsg = getString(R.string.no_difference);
+                    else resultMsg = getString(R.string.fuel_2_better, efficiency.toString());
+
+                    builder.setMessage(resultMsg).setTitle(R.string.result).setPositiveButton("OK"){dialog, which ->};
+                    internDialog = builder.create();
+                    internDialog.show();
+
+                }else{
+                    if (efficiency == 0) resultMsg = getString(R.string.no_difference);
+                    else resultMsg = getString(R.string.fuel_1_better, efficiency.toString());
+
+                    builder.setMessage(resultMsg).setTitle(R.string.result).setPositiveButton("OK"){dialog, which ->};
+                    internDialog = builder.create();
+                    internDialog.show();
+                }
+
+            };
+            val dialog: AlertDialog = builder.create();
+            dialog.show();
+
+
+
+
+        }else{
+            if (fuelPriceRatio <= fuelAutonomyRatio){  //Se o custo do etanol for de aproximadamente 0,7 ou 70% do valor da gasolina, saberá com o calculo anterior
+                if (efficiency == 0) resultMsg = getString(R.string.no_difference);
+                else resultMsg = getString(R.string.ethanol_better, efficiency.toString());
+
+                builder.setMessage(resultMsg).setTitle(R.string.result).setPositiveButton("OK") { dialog, which -> };
+                val dialog: AlertDialog = builder.create();
+                dialog.show();
+
+            }else{
+                if (efficiency == 0) resultMsg = getString(R.string.no_difference);
+                else resultMsg  = getString(R.string.gasoline_better, efficiency.toString());
+                builder.setMessage(resultMsg).setTitle(R.string.result).setPositiveButton("OK") { dialog, which -> };
+                val dialog: AlertDialog = builder.create();
+                dialog.show();
+            }
+        }
+
+
+
+
+
+    }
+
+
+
 
 
 
